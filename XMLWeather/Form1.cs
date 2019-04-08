77da74fc232e5ab0bf17bf5e75cc7e0cd5a7044c
jamasouk354 <jamasouk354@SCS-D-360QJ2>
2019-04-08 09:59:58 -0400

    Progress

--- XMLWeather/Form1.cs
+++ XMLWeather/Form1.cs
@@ -13,8 +13,7 @@ namespace XMLWeather
 {
     public partial class Form1 : Form
     {
-        // TODO: create list to hold day objects
-
+        public static List<Day> days = new List<Day>();
 
         public Form1()
         {
@@ -34,11 +33,18 @@ namespace XMLWeather
 
             while (reader.Read())
             {
-                //TODO: create a day object
-
+                Day d = new Day();
                 //TODO: fill day object with required data
-
+                reader.ReadToFollowing("time");
+                d.date = reader.GetAttribute("day");
+                reader.ReadToFollowing("temperature");
+                d.tempLow = reader.GetAttribute("min");
+                d.tempHigh = reader.GetAttribute("max");
                 //TODO: if day object not null add to the days list
+                if (d.date != null)
+                {
+                    days.Add(d);
+                }
             }
         }
 
@@ -48,9 +54,10 @@ namespace XMLWeather
             XmlReader reader = XmlReader.Create("http://api.openweathermap.org/data/2.5/weather?q=Stratford,CA&mode=xml&units=metric&appid=3f2e224b815c0ed45524322e145149f0");
 
             //TODO: find the city and current temperature and add to appropriate item in days list
-
+            reader.ReadToFollowing("city");
+            days[0].location = reader.GetAttribute("name");
+            reader.ReadToFollowing("temperature");
+            days[0].currentTemp = reader.GetAttribute("value");
         }
-
-
     }
 }
